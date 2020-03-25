1f51d38023f96073b00624f09210a870e664abb2
Mike Kruk <mike@ripeworks.com>
2020-03-25 14:17:48 -0400

    fixes

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const dotEnv = require("dotenv");
-const fs = require("fs");
-const sysPath = require("path");
 const process = require("process");
+
+let envConfig;
+
+const loadConfig = path => {
+  if (!path) return {};
+  const env = dotEnv.config({ path, silent: true });
+
+  return env ? env.parsed : {};
+};
 
 module.exports = function(data) {
   const t = data.types;
@@ -14,19 +21,17 @@ module.exports = function(data) {
         if (options.moduleName === undefined) return;
 
         const envs = options.envs || {};
-        const defaultEnvFile = envs.default;
-        const envFile = envs[process.env.APP_ENV];
+        const defaultEnvFile = envs.default || "";
+        const envFile = envs[process.env.APP_ENV || "development"] || "";
 
         if (path.node.source.value === options.moduleName) {
-          const defaultConfig =
-            dotEnv.config({
-              path: sysPath.join(defaultEnvFile),
-              silent: true
-            }) || {};
-          const envConfig = Object.assign(
-            defaultConfig,
-            dotEnv.config({ path: sysPath.join(envFile), silent: true })
-          );
+          if (!envConfig) {
+            envConfig = Object.assign(
+              {},
+              loadConfig(defaultEnvFile),
+              loadConfig(envFile)
+            );
+          }
 
           path.node.specifiers.forEach(function(specifier, idx) {
             if (specifier.type === "ImportDefaultSpecifier") {
@@ -40,21 +45,21 @@ module.exports = function(data) {
             const importedId = specifier.imported.name;
             const localId = specifier.local.name;
 
-            if (!config.hasOwnProperty(importedId)) {
+            if (!envConfig.hasOwnProperty(importedId)) {
               throw path
                 .get("specifiers")
                 [idx].buildCodeFrameError(
                   'Try to import dotenv variable "' +
                     importedId +
                     '" which is not defined in any ' +
-                    configFile +
+                    (defaultEnvFile || envFile) +
                     " files."
                 );
             }
 
             const binding = path.scope.getBinding(localId);
             binding.referencePaths.forEach(function(refPath) {
-              refPath.replaceWith(t.valueToNode(config[importedId]));
+              refPath.replaceWith(t.valueToNode(envConfig[importedId]));
             });
           });
 
