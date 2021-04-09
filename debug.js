8d47cb2075c07497aa3828a21cef93cd4f8a74e6
Mike Kruk <mike@ripeworks.com>
2021-04-09 12:11:13 -0400

    use fs to read environments file

--- debug.js
+++ debug.js
@@ -1,9 +1,12 @@
+const fs = require("fs");
+
 let appEnv = process.env.APP_ENV || "production";
 let appConfig;
 
 if (process.env.APP_ENV !== "production") {
   try {
-    appConfig = require("./environments.json");
+    const envFile = fs.readFileSync("./environments.json");
+    appConfig = JSON.parse(envFile);
   } catch (err) {
     throw new Error(
       `@slytrunk/app-env: No environments.json found. Did you include the babel plugin?`
